9a1c7e8102ea228966e78a4689081350aa698882
tsei902 <93079818+tsei902@users.noreply.github.com>
2023-03-09 16:12:13 +0100

    tensor formatting of generator

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # model.resize_token_embeddings(len(tokenizer))
 # model = AutoModelForSeq2SeqLM.from_pretrained("./content/drive/My Drive/Transformers/t5-base-dutch") # , from_pt=False)
 import wandb
-# from torch import cuda
+from torch import cuda
 import torch
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer, set_seed
 # , T5ForConditionalGeneration, TrainingArguments
@@ -130,7 +130,7 @@ class T5SimplificationModel():
 def get_device():
     return 'cuda' if cuda.is_available() else 'cpu'
 
-def preprocess_function(examples):
+def preprocess_function_train(examples):
     # https://medium.com/nlplanet/a-full-guide-to-finetuning-t5-for-text2text-and-building-a-demo-with-streamlit-c72009631887
     max_input_length = 256
     max_target_length = 256
@@ -150,9 +150,25 @@ def preprocess_function(examples):
     # for labels_example in labels:
     #   labels_example = [label if label != 0 else -100 for label in labels_example]
     #   labels_with_ignore_index.append(labels_example)
+    
+    #       # important: we need to replace the index of the padding tokens by -100
+    #   # such that they are not taken into account by the CrossEntropyLoss
+    #   labels_with_ignore_index = []
+    #   for labels_example in labels:
+    #     labels_example = [label if label != 0 else -100 for label in labels_example]
+    #     labels_with_ignore_index.append(labels_example)
+    # model_inputs["labels"] = labels_with_ignore_index
 
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
+
+def preprocess_function_test(example):
+    # https://medium.com/nlplanet/a-full-guide-to-finetuning-t5-for-text2text-and-building-a-demo-with-streamlit-c72009631887
+    
+    max_length = 256
+    input_ids = tokenizer(example, max_length=max_length, return_tensors="pt") # ,truncation=True) #, return_tensors="pt") # padding=True , )  # return_tensors="pt"
+    return input_ids
+
 
 # model_name = model_checkpoint.split("/")[-1]
 training_args = Seq2SeqTrainingArguments(
@@ -161,7 +177,7 @@ training_args = Seq2SeqTrainingArguments(
         learning_rate=0.001,
         per_device_train_batch_size=8,
         per_device_eval_batch_size=8,
-        predict_with_generate=True,
+        predict_with_generate=True, # use model for eval
         num_train_epochs=3,
         gradient_accumulation_steps=4,
         # gradient_checkpointing=True,
@@ -172,74 +188,73 @@ training_args = Seq2SeqTrainingArguments(
         # evaluation and logging
         evaluation_strategy = "epoch", # needs to remain epoch otherwise no tracking of training loss!
         save_strategy = "epoch",
-        save_total_limit=1,
+        save_total_limit=3,
         logging_strategy="epoch",
         # logging_steps = 1, 
         load_best_model_at_end=True,
+        
         metric_for_best_model = "eval_loss",
         # use_cache=False,
         push_to_hub=False,
         fp16=False, # True, # shorter bits, more efficient # tensorsneed to be a multiple of 8 # only savings with high batch size
-        output_dir="./output/"
+        output_dir="./model_output/"
     )
-def encoding_test():
+def encoding_test(dataset):
     # sentence 1
     print("sentence 1")
-    test_sent1= preprocess_function(dataset['train'][1])  ##issue too short! müsste viel länger sein, weil der Paragraph auch viel länger ist!
-    # print(test_sent1) 
+    test_sent1= preprocess_function_train(dataset['train'][1])  ##issue too short! müsste viel länger sein, weil der Paragraph auch viel länger ist!
+    print(test_sent1) 
     print("input_sentence: ", tokenizer.decode(test_sent1["input_ids"]))
     print("labels: ", tokenizer.decode(test_sent1["labels"]))
     # sentence 2
     print("sentence 2")
-    test_sent2 = preprocess_function(dataset['train'][2])
-    # print(test_sent2)
+    test_sent2 = preprocess_function_train(dataset['train'][2])
+    print(test_sent2)
     print("input_sentence: ", tokenizer.decode(test_sent2["input_ids"]))
     print("labels: ", tokenizer.decode(test_sent2["labels"]))
     # sentence 3
     print("sentence 3")
-    test_sent3 = preprocess_function(dataset['train'][3])
-    # print(test_sent3)
+    test_sent3 = preprocess_function_train(dataset['train'][3])
+    print(test_sent3)
     print("input_sentence: ", tokenizer.decode(test_sent3["input_ids"]))
     print("labels: ", tokenizer.decode(test_sent3["labels"]))
     # sentence 4
     print("sentence 4")
-    test_sent4 = preprocess_function(dataset['train'][4])
-    # print(test_sent4)
+    test_sent4 = preprocess_function_train(dataset['train'][4])
+    print(test_sent4)
     print("input_sentence: ", tokenizer.decode(test_sent4["input_ids"]))
     print("labels: ", tokenizer.decode(test_sent4["labels"]))
     
-def generate(test_dataset, trained_model, tokenizer):
-    input_ids =  tokenizer(test_dataset['orig.dutch'][2], return_tensors="pt").input_ids
-    print('original: ', test_dataset['orig.dutch'][2])
-    # afterwards simplified sentences need tokenization as well 
-    # print(len(orig_sent.split()), " words")
-    output = trained_model.generate(
-            input_ids, 
-            do_sample=False, # sampling method makes errors 
-            # min_new_tokens=13,
-            max_new_tokens=40, # longer is better!! # max_target_length, #128 # countOfWords as alternative
-            # doesnt work?  
-            # top_k=0, # either temperature or top_k
-            # temperature=0.7,  # more weight to powerful tokens
-            # # remove_invalid_values=True
-            # num_beams = 8,# preset
-            # early_stopping= True,
-            # length_penalty= 2.0,
-            # top_p=0.9, # top p of probability distribution
-            # top_k=2,
-            # temperature=0.8,
-            # min_length= 30,
-            # no_repeat_ngram_size= 3,
-            num_beams= 4,
-            )
+def generate(tokenized_test_input, trained_model, tokenizer): #, output_attentions=True
+     
+    output = trained_model.generate( tokenized_test_input,  
+                do_sample=False, # sampling method makes errors 
+                # min_new_tokens=13,
+                max_new_tokens=40, # longer is better!! # max_target_length, #128 # countOfWords as alternative
+                # doesnt work?  
+                # top_k=0, # either temperature or top_k
+                # temperature=0.7,  # more weight to powerful tokens
+                # # remove_invalid_values=True
+                # num_beams = 8,# preset
+                # early_stopping= True,
+                # length_penalty= 2.0,
+                # top_p=0.9, # top p of probability distribution
+                # top_k=2,
+                # temperature=0.8,
+                # min_length= 30,
+                # no_repeat_ngram_size= 3,
+                num_beams= 4,
+                )
   
+    # try tokenizer.batck_decode(simplification)
     simplification = tokenizer.decode(output.squeeze(), skip_special_tokens=False)
-    with open("simplification.txt", "w") as f:
-        for word in simplification:
-            f.write(word)
-    lensimpl = len(simplification.split())
-    print(lensimpl, " words")
-    simplification.replace('. ', '.\n')
+    file=open("./resources/outputs/generate/simplification.txt", "r+") 
+    file.writelines(simplification)
+    file.close()
+
+    # lensimpl = len(simplification.split())
+    # print(lensimpl, " words")
+    # simplification.replace('. ', '.\n')
     
     return simplification
     
@@ -289,16 +304,16 @@ def generate(test_dataset, trained_model, tokenizer):
 if __name__ == '__main__':
     # wandb.login()  
     # wandb.init(project="dutch_simplification")   #wandb.log({'accuracy': train_acc, 'loss': train_loss})
-    # model_checkpoint = "yhavinga/t5-base-dutch" #"yhavinga/t5-v1.1-base-dutch-cased" #"flax-community/t5-base-dutch"#
-    # tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-    # model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint, gradient_checkpointing=True, use_cache=False)
+    model_checkpoint = "yhavinga/t5-base-dutch" #"yhavinga/t5-v1.1-base-dutch-cased" #"flax-community/t5-base-dutch"#
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint, gradient_checkpointing=True, use_cache=False)
     # wandb.watch(model, log="all")
     # dataset= get_data_txt(WIKILARGE_DATASET, 10)  
     # print(dataset['train'][3])  
     # tokenized_datasets = dataset.map(preprocess_function, batched=True)
     # print(tokenized_datasets)
     # time.sleep(7)
-    # tests= encoding_test()
+    # tests= encoding_test(dataset)
     # data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
     # trainer = Seq2SeqTrainer(model=model,args=training_args,train_dataset=tokenized_datasets["train"],
     #     eval_dataset=tokenized_datasets["validation"], # should be validation!!
@@ -315,14 +330,24 @@ if __name__ == '__main__':
     # print(model)
     print('./saved_model/training_args')
     
-
     # GENERATION
     test_dataset = get_test_data_txt(ASSET_DATASET, 10)
-    print(test_dataset)
-    generated_dataset= generate(test_dataset, trained_model, tokenizer)
+    # print('orig dutch of test set: ' ,test_dataset['orig.dutch'])
+    # test_sent3= test_dataset['orig.dutch'][3]
+    # print('test sent 3 :', test_sent3)
+    # test_sent3 = preprocess_function_test(test_dataset['orig.dutch'][3])
+    # print(test_sent3)
+    # test_sent3['input_ids']
+    # print("input_sentence: ", tokenizer.decode(test_sent3['input_ids']))
+    # # print("labels: ", tokenizer.decode(test_sent3))
+    print(type(test_dataset['orig.dutch'])) # list of strings 
+    
+    print(type(test_dataset['orig.dutch'][2])) # string
+    tokenized_test_input = preprocess_function_test(test_dataset['orig.dutch'][2])
+    print('tokenized test input from 2', tokenized_test_input)
+    print('tokenized test input from 2 INPUT IDS: ',tokenized_test_input['input_ids'])
+    print(type(tokenized_test_input['input_ids'])) # is handled as a list and not a tensor! 
+    generated_dataset= generate(tokenized_test_input['input_ids'], trained_model, tokenizer)
     print(generated_dataset)
-    # load weights
-    # dataset= get_data_txt(ASSET_DATASET, 10)
-    # tokenized_datasets = dataset.map(preprocess_function, batched=True) # concatenation only for datasets, we have datasetdict # remove_columns=['herkomst', 'eenvoudig1']
-    # print(tokenized_datasets)
+
     
